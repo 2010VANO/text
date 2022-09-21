e84a65d2055b0fb1a4ce0960dd3b55a60e850d5d
IVAN <ivanvasilkov2004@mail.ru>
2022-09-21 09:17:25 +0300

    add file

---
+++ venv/main.py
@@ -0,0 +1,7 @@
+i Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.

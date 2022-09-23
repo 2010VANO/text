f69f8913c5dfb7c7e10e003973f560c21833c36a
IVAN <ivanvasilkov2004@mail.ru>
2022-09-23 11:43:00 +0300

    add file

---
+++ main.py
@@ -0,0 +1,28 @@
+name = "IVAN"
+
+print("2.3")
+print("Hello,", name+\
+      ", it's ur homework")
+
+name = "        ivan   / "
+print("2.4")
+print(name.lower() ,"все буквы в нижнем регистре")
+print(name.upper(),"все буквы в верхнем регистре")
+print(name.title(),"первая буква заглавная")
+
+print("2.5")
+print("Alexander Pushkin once said,  'Poetry, God forgive me, must be a bit silly'")
+
+print("2.6")
+name = "Alexander Pushkin"
+print(name, "once said,  'Poetry, God forgive me, must be a bit silly'")
+
+print("2.7")
+word = "\tabrikos\nfonarik"
+print(word)
+print(str.strip(word))
+print(str.lstrip(word))
+print(str.rstrip(word))
+
+
+
